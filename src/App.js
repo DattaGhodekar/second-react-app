f4426af42df7b56c2eebeef72b3120665948a4c7
datta ghodekar <dattatray2251998@gmail.com>
2023-06-14 11:52:38 +0530

    jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 function App () {
-  return (
-  <>
-  <h1>hello</h1>
-  <h2>world</h2>
-  </>
-  )
+
+return (
+
+<h1>
+  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident, consequatur ab! Neque, itaque officia, facere quos consequatur illo modi porro tenetur mollitia minus totam repudiandae ipsa quasi ab error sapiente?
+</h1>
+)
 }
 export default App;
