95159e3361041f4de8534d9725e0e68318892b53
datta ghodekar <dattatray2251998@gmail.com>
2023-06-14 11:56:54 +0530

    syntas2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 function App () {
-
-return (
-
-<h1>
-  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident, consequatur ab! Neque, itaque officia, facere quos consequatur illo modi porro tenetur mollitia minus totam repudiandae ipsa quasi ab error sapiente?
-</h1>
-)
+let jsx1 = <h1>hello univers</h1>
+return jsx1;
 }
 export default App;
